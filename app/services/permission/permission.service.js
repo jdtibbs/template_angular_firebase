31c474df08dc388fb6ae36bac586bf8f02bb9abf
Jack Tibbs <jdtibbs@users.noreply.github.com>
2015-09-23 04:03:50 -0500

    roles and permissions work in progress

--- app/services/permission/permission.service.js
+++ app/services/permission/permission.service.js
@@ -10,17 +10,25 @@
     function serviceFn(loginService, $log, roleService, rx) {
         this.hasPermission = hasPermission;
 
-        function hasPermission() {
-            if (roleService.hasPermission()) {
-                return ture;
-            } else if (permission()) {
+        // TODO: idea... use firebase read rule to determine if user can access given route.
+        // see: https://www.firebase.com/docs/security/guide/securing-data.html#section-other-paths
+        // route:{read: root.child('') ... }
+        function hasPermission(route) {
+            // get permissions required for access to the given route/page.
+
+            if (has()) {
+                // user can be given specific permissions.
+                return true;
+            } else if (roleService.hasPermission()) {
+                // user can be given multiple roles, roles can be given multiple permissions.
                 return true;
             } else {
+                // user is not permitted to access the specified page.
                 return false;
             }
         }
 
-        function permission() {
+        function has() {
             return true;
         }
     }
