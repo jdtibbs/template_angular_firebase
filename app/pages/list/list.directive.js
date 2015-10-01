1a36b22bf4b509d8835bd5448c927b03b8456042
Jack Tibbs <jdtibbs@users.noreply.github.com>
2015-10-01 17:18:18 -0500

    list click dialog work in progress.

--- app/pages/list/list.directive.js
+++ app/pages/list/list.directive.js
@@ -4,9 +4,9 @@
 	angular.module('login.module')
 		.directive('jdtList', directiveFn);
 
-	directiveFn.$inject = ['feedbackFactory', 'listConstants', 'listDaoFactory', '$log', 'rx'];
+	directiveFn.$inject = ['feedbackFactory', 'listConstants', 'listDaoFactory', '$log', '$mdDialog', 'rx'];
 
-	function directiveFn(feedbackFactory, listConstants, listDaoFactory, $log, rx) {
+	function directiveFn(feedbackFactory, listConstants, listDaoFactory, $log, $mdDialog, rx) {
 		return {
 			restrict: 'E',
 			scope: {
@@ -23,6 +23,8 @@
 			var vm = this;
 			vm.props.title = listConstants.title;
 			vm.feedback = {};
+			vm.click = click;
+
 			var feedback = feedbackFactory(vm.feedback);
 
 			// RxJS, just tinkering.
@@ -40,6 +42,45 @@
 
 			function onComplete() {
 				// $log.debug('rx fromCallbak complete');
+			}
+
+			function click(key) {
+				$log.debug('clicked: ' + key);
+				showDialog();
+			}
+
+			function showDialog() {
+				var parentEl = angular.element(document.body);
+				$mdDialog.show({
+					parent: parentEl,
+					//targetEvent: $event,
+					template: '<md-dialog aria-label="List dialog">' +
+						'  <md-dialog-content>' +
+						'    <md-list>' +
+						'      <md-list-item ng-repeat="item in items">' +
+						'       <p>Number {{item}}</p>' +
+						'      ' +
+						'    </md-list-item></md-list>' +
+						'  </md-dialog-content>' +
+						'  <div class="md-actions">' +
+						'    <md-button ng-click="closeDialog()" class="md-primary">' +
+						'      Close Dialog' +
+						'    </md-button>' +
+						'  </div>' +
+						'</md-dialog>',
+					locals: {
+						//items: $scope.items
+					},
+					controller: DialogController
+				});
+
+				// function DialogController($scope, $mdDialog, items) {
+				function DialogController() {
+					// $scope.items = items;
+					// $scope.closeDialog = function() {
+					// $mdDialog.hide();
+					// };
+				}
 			}
 		}
 
