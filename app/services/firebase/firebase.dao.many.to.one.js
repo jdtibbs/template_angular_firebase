6ca84f46cd367f58e71e9b67ba540a4d9f2d2d9b
Jack Tibbs <jdtibbs@users.noreply.github.com>
2016-01-04 15:16:53 -0600

    refactor daoManyToOne to be object.

--- app/services/firebase/firebase.dao.many.to.one.js
+++ app/services/firebase/firebase.dao.many.to.one.js
@@ -9,113 +9,115 @@
 
     function factoryFn($firebaseArray, $firebaseObject, firebaseDao, firebaseRulesFactory, routeParamsFactory, $log) {
 
-        var add = function(object, callback, feedback) {
-            var oneKey = routeParamsFactory.getParam(this.oneConstant.dao);
-            var oneRefChild = this.ref().child(this.oneConstant.dao).child(oneKey).child(this.constant.dao);
-            var rulesFactory = firebaseRulesFactory(this.constant, oneRefChild);
+        var dao = {
+            add: function(object, callback, feedback) {
+                var oneKey = routeParamsFactory.getParam(this.oneConstant.dao);
+                var oneRefChild = this.ref().child(this.oneConstant.dao).child(oneKey).child(this.constant.dao);
+                var rulesFactory = firebaseRulesFactory(this.constant, oneRefChild);
 
-            var add = function() {
-                var newRef = this.ref().child(this.constant.dao).push();
-                var newKey = newRef.key();
+                var add = function() {
+                    var newRef = this.ref().child(this.constant.dao).push();
+                    var newKey = newRef.key();
+                    var newData = {};
+
+                    newData[this.oneConstant.dao + '/' + oneKey + '/' + this.constant.dao + '/' + newKey] = true;
+                    newData[this.constant.dao + '/' + newKey] = object;
+
+                    var onComplete = function(error) {
+                        if (error) {
+                            $log.error(error);
+                            feedback.error('Error writing ' + this.constant.title + '.');
+                        } else {
+                            feedback.success(this.constant.title + ' saved successfully.');
+                            callback(newKey);
+                        }
+                    }.bind(this);
+
+                    this.ref().update(newData, onComplete);
+                }.bind(this);
+
+                function isWithinAddLimitCallback(isWithinAddLimit) {
+                    if (isWithinAddLimit) {
+                        add();
+                    }
+                }
+
+                function isAddLimitCallback(isAddLimit) {
+                    if (isAddLimit) {
+                        rulesFactory.isWithinAddLimit(feedback, isWithinAddLimitCallback);
+                    } else {
+                        add();
+                    }
+                }
+
+                function canAddCallback(canAdd) {
+                    if (canAdd) {
+                        rulesFactory.isAddLimit(feedback, isAddLimitCallback);
+                    }
+                }
+
+                rulesFactory.canAdd(feedback, canAddCallback);
+            },
+
+            remove: function(key, feedback) {
+                var oneKey = routeParamsFactory.getParam(this.oneConstant.dao);
                 var newData = {};
 
-                newData[this.oneConstant.dao + '/' + oneKey + '/' + this.constant.dao + '/' + newKey] = true;
-                newData[this.constant.dao + '/' + newKey] = object;
+                newData[this.oneConstant.dao + '/' + oneKey + '/' + this.constant.dao + '/' + key] = null;
+                newData[this.constant.dao + '/' + key] = null;
 
                 var onComplete = function(error) {
                     if (error) {
                         $log.error(error);
-                        feedback.error('Error writing ' + this.constant.title + '.');
+                        feedback.error('Error deleting ' + this.constant.title + '.');
                     } else {
-                        feedback.success(this.constant.title + ' saved successfully.');
-                        callback(newKey);
+                        feedback.success(this.constant.title + ' deleted successfully.');
                     }
                 }.bind(this);
 
                 this.ref().update(newData, onComplete);
-            }.bind(this);
+            },
 
-            function isWithinAddLimitCallback(isWithinAddLimit) {
-                if (isWithinAddLimit) {
-                    add();
-                }
-            }
+            syncArray: function(key, data, feedback) {
+                var keyRef = this.ref().child(this.oneConstant.dao)
+                    .child(key)
+                    .child(this.constant.dao);
 
-            function isAddLimitCallback(isAddLimit) {
-                if (isAddLimit) {
-                    rulesFactory.isWithinAddLimit(feedback, isWithinAddLimitCallback);
-                } else {
-                    add();
-                }
-            }
+                var manyKeys = $firebaseArray(keyRef);
 
-            function canAddCallback(canAdd) {
-                if (canAdd) {
-                    rulesFactory.isAddLimit(feedback, isAddLimitCallback);
-                }
-            }
-
-            rulesFactory.canAdd(feedback, canAddCallback);
-        };
-
-        var remove = function(key, feedback) {
-            var oneKey = routeParamsFactory.getParam(this.oneConstant.dao);
-            var newData = {};
-
-            newData[this.oneConstant.dao + '/' + oneKey + '/' + this.constant.dao + '/' + key] = null;
-            newData[this.constant.dao + '/' + key] = null;
-
-            var onComplete = function(error) {
-                if (error) {
-                    $log.error(error);
-                    feedback.error('Error deleting ' + this.constant.title + '.');
-                } else {
-                    feedback.success(this.constant.title + ' deleted successfully.');
-                }
-            }.bind(this);
-
-            this.ref().update(newData, onComplete);
-        };
-
-        var syncArray = function(key, data, feedback) {
-            var keyRef = this.ref().child(this.oneConstant.dao)
-                .child(key)
-                .child(this.constant.dao);
-
-            var manyKeys = $firebaseArray(keyRef);
-
-            var watchFn = function(event) {
-                if (event.event === 'child_added') {
-                    $firebaseObject(this.ref().child(this.constant.dao).child(event.key)).$loaded()
-                        .then(function(obj) {
-                            data.push(obj);
-                        }).catch(function(error) {
-                            $log.error(error);
-                            feedback.error('Error reading ' + this.oneConstant.title + ' & ' + this.constant.title + '.');
+                var watchFn = function(event) {
+                    if (event.event === 'child_added') {
+                        $firebaseObject(this.ref().child(this.constant.dao).child(event.key)).$loaded()
+                            .then(function(obj) {
+                                data.push(obj);
+                            }).catch(function(error) {
+                                $log.error(error);
+                                feedback.error('Error reading ' + this.oneConstant.title + ' & ' + this.constant.title + '.');
+                            });
+                    }
+                    if (event.event === 'child_removed') {
+                        data.findIndex(function(obj, index, array) {
+                            if (obj.$id === event.key) {
+                                array.splice(index, 1);
+                            }
                         });
-                }
-                if (event.event === 'child_removed') {
-                    data.findIndex(function(obj, index, array) {
-                        if (obj.$id === event.key) {
-                            array.splice(index, 1);
-                        }
-                    });
-                }
-            }.bind(this);
+                    }
+                }.bind(this);
 
-            manyKeys.$watch(watchFn);
+                manyKeys.$watch(watchFn);
+            }
         };
 
         // create the prototype.
         var objectDescriptor = {
             add: {
-                value: add
+                value: dao.add
             },
             remove: {
-                value: remove
+                value: dao.remove
             },
             syncArray: {
-                value: syncArray
+                value: dao.syncArray
             }
         };
         return Object.create(firebaseDao, objectDescriptor);
