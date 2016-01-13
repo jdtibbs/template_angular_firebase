fbd7af5307b8aa6d6284d2c5994d2b3880acba32
Jack Tibbs <jdtibbs@users.noreply.github.com>
2016-01-12 21:18:39 -0600

    add requireAuth to routes

--- app/pages/vendor/edit/vendor.edit.config.route.js
+++ app/pages/vendor/edit/vendor.edit.config.route.js
@@ -8,10 +8,22 @@
 
 	function config(vendorConstantsProvider, $routeProvider) {
 		$routeProvider.when(vendorConstantsProvider.pathEdit() + vendorConstantsProvider.dao() + '/:' + vendorConstantsProvider.dao(), {
-				templateUrl: 'app/pages/vendor/edit/vendor.edit.container.html'
+				templateUrl: 'app/pages/vendor/edit/vendor.edit.container.html',
+				resolve: {
+					requireAuth: requireAuth
+				}
 			})
 			.when(vendorConstantsProvider.pathAdd(), {
-				templateUrl: 'app/pages/vendor/edit/vendor.edit.container.html'
+				templateUrl: 'app/pages/vendor/edit/vendor.edit.container.html',
+				resolve: {
+					requireAuth: requireAuth
+				}
 			});
+	}
+
+	requireAuth.$inject = ['loginService'];
+
+	function requireAuth(loginService) {
+		return loginService.requireAuth();
 	}
 })();
--- app/pages/vendor/list/vendor.list.config.route.js
+++ app/pages/vendor/list/vendor.list.config.route.js
@@ -8,8 +8,17 @@
 
 	function config(vendorConstantsProvider, $routeProvider) {
 		$routeProvider.when(vendorConstantsProvider.path(), {
-			templateUrl: 'app/pages/vendor/list/vendor.list.container.html'
+			templateUrl: 'app/pages/vendor/list/vendor.list.container.html',
+			resolve: {
+				requireAuth: requireAuth
+			}
 		});
+	}
+
+	requireAuth.$inject = ['loginService'];
+
+	function requireAuth(loginService) {
+		return loginService.requireAuth();
 	}
 
 
